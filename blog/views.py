ae44ee15abdc5a04e5d1dcabe4cbfabf05709c9f
wasibani-roy <roywaisibani@gmail.com>
2019-01-03 19:19:14 +0300

    My first work on blog django project

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author':'roy',
+        'title': 'blog post 1',
+        'content': 'first post content',
+        'date_posted': 'January 3 2019'
+    },
+    {
+        'author':'wasibani',
+        'title': 'blog post 2',
+        'content': 'second post content',
+        'date_posted': 'January 4 2019'
+    }
+]
+
+def home(request):
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
